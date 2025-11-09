fc2a31b092157cd73b3137e201f68277c802b4b3
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-09 21:56:12 +0530

    Array-Method

--- script.js
+++ script.js
@@ -13,5 +13,39 @@
 //  const abcd= document.querySelector('body')
 //   abcd.style.background="lightseagreen"
 
+//Practice Question Array Method
+ // // Array Method
+// // 1-Push
+// // 2-Pop
+// // 3-shift
+// // 4-unshift
+// // 5-filter
+// // 6-find
 
- 
+// // 1-Push
+
+// let ejaz=["apple","banana","mango","orange",1,2,3]
+// console.log(ejaz,"print")
+// ejaz.push("manzar")
+// console.log(ejaz,"after push")
+
+// // 2-pop
+
+// ejaz.pop()
+// console.log(ejaz,"after pop")
+
+// // 3-Shift
+
+// ejaz.shift()
+// console.log(ejaz,"after shift")
+
+// // 3-UnShift
+
+// ejaz.unshift("grapes")
+// console.log(ejaz,"after unshift")
+
+// // 4-filter
+// let delhi=[1,2,3,"manzar","dilsahd","dilsahd", "nisar","dilsahd"]
+
+// let one = delhi.filter(ejaz=> ejaz=="dilsahd")
+// console.log(one ,"print")
