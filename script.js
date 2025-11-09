0393063c873ff78a3149198536eb70a827ffebf6
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-09 22:33:17 +0530

    Question-Break$Continue

--- script.js
+++ script.js
@@ -155,3 +155,15 @@
 // while(pass!==userpasswowrd){
 //    userpasswowrd=prompt("sahi password bataooo");
 // } 
+
+//Question Break$Continue
+//Break
+// for(let i=1; i<10; i++){
+//     if(i===5)break;
+//     console.log(i)
+// }
+//Continue
+// for(let i=1; i<10; i++){
+//     if(i===5)continue;
+//     console.log(i)
+// }
