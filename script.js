5300d95d9e225ad06742905aa616d1954b38b6b6
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-09 21:53:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+//Practice Question 1
+//    const abc = "hello my name is ejaz ahmad"
+//    console.log(abc)
+
+////Practice Question 2
+//    document.querySelectorAll('h1')
+//    alert("hello")
+
+////Practice Question 3
+//  const abc= document.querySelector('h1')
+//  abc.style.color="darkblue"
+
+//  const abcd= document.querySelector('body')
+//   abcd.style.background="lightseagreen"
+
+
+ 
