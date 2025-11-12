9604b060481067b3c76101dd3e2d13b42dae4abc
ejazahmad18 <numen.ejaz@gmail.com>
2025-11-12 16:51:47 +0530

    Array-reduce

--- script.js
+++ script.js
@@ -507,10 +507,16 @@
 
 
 // Array ( filter )
-   let arr=[1,2,3,4,5];
-   let newarr=arr.filter(function(val){
-    // return true;
-      if(val>0) return true;
-  });
+//    let arr=[1,2,3,4,5];
+//    let newarr=arr.filter(function(val){
+//     // return true;
+//       if(val>0) return true;
+//   });
 
+
+// Array ( reduce )
+   let arr=[1,2,3,4,5];
+   let newarr=arr.reduce(function(accumulator , val){
+    return accumulator+ val;
+  },0);
 
